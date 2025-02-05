1fbb2fcace2c034fac8eb465c9221a512a283f2c
Longnewbie <lygialong201@gmail.com>
2025-02-05 16:40:33 +0700

    Implementing Socket.io for Realtime Features

--- fe/src/providers/AuthProvider.tsx
+++ fe/src/providers/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@/lib/axios";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
 import { useAuth } from "@clerk/clerk-react";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -13,9 +14,10 @@ const updateApiToken = (token: string | null) => {
 };
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { getToken } = useAuth();
+  const { getToken, userId } = useAuth();
   const [loading, setLoading] = useState(true);
   const { checkAdminStatus } = useAuthStore();
+  const { initSocket, disconnectSocket } = useChatStore();
 
   useEffect(() => {
     const initAuth = async () => {
@@ -24,6 +26,10 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         updateApiToken(token);
         if (token) {
           await checkAdminStatus();
+          // init socket
+          if (userId) {
+            initSocket(userId);
+          }
         }
       } catch (error: any) {
         updateApiToken(null);
@@ -34,7 +40,12 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     };
 
     initAuth();
-  }, [getToken]);
+
+    // cleanup function to disconnect socket
+    return () => {
+      disconnectSocket();
+    };
+  }, [getToken, userId, checkAdminStatus, initSocket, disconnectSocket]);
 
   if (loading) {
     return (
